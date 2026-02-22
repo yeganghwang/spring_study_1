599c07b660cffe873676668e08f299bb96e00509
yeganghwang <ghkdtlahs@icloud.com>
2026-02-22 22:47:53 +0900

    브랜치 테스트1

--- src/main/java/com/example/study_1/controller/HelloController.java
+++ src/main/java/com/example/study_1/controller/HelloController.java
@@ -8,7 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
+        model.addAttribute("title", "hello");
         model.addAttribute("data", "hello!!!!!!!!");
+        model.addAttribute("korean", "안녕하세요");
+        return "hello";
+    }
+
+    @GetMapping("bye")
+    public String bye(Model model) {
+        model.addAttribute("title", "bye");
+        model.addAttribute("data", "bye!!!!!!!!");
+        model.addAttribute("korean", "안녕히 가세요");
         return "hello";
     }
 }
