a36923df47953329eaae3cacfce348d0df4a5e2f
yeganghwang <ghkdtlahs@icloud.com>
2026-05-22 13:54:30 +0900

    테스트

--- src/test/java/com/yegangs/study2/Study2ApplicationTests.java
+++ src/test/java/com/yegangs/study2/Study2ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.yegangs.study2;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class Study2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    @Transactional
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+        answerList.forEach(answer -> {
+           System.out.println(answer.getContent());
+        });
+        assertEquals("네, 자동으로 생성됩니다.", q.getAnswerList().getFirst().getContent());
+    }
 }
