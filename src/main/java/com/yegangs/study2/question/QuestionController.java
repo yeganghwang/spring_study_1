df89724dda068d7b24560b12ee76fa3f5cad7dd1
yeganghwang <ghkdtlahs@icloud.com>
2026-05-22 14:15:18 +0900

    질문 페이지

---
+++ src/main/java/com/yegangs/study2/question/QuestionController.java
@@ -0,0 +1,25 @@
+package com.yegangs.study2.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("question")
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+
+}
