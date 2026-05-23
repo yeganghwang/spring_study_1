09db43a4459a16641e6316eb9f2f6f1dd2936e55
yeganghwang <ghkdtlahs@icloud.com>
2026-05-23 16:17:44 +0900

    질문 등록 기능 추가

--- src/main/java/com/yegangs/study1/question/QuestionController.java
+++ src/main/java/com/yegangs/study1/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.yegangs.study1.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,17 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam("subject") String subject,
+                                 @RequestParam("content") String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/yegangs/study1/question/QuestionService.java
+++ src/main/java/com/yegangs/study1/question/QuestionService.java
@@ -3,6 +3,7 @@ package com.yegangs.study1.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
