a53de76a0ae895e81ef28fa93b1c179638298232
yeganghwang <ghkdtlahs@icloud.com>
2026-05-25 14:32:56 +0900

    회원가입 기능 추가

---
+++ src/main/java/com/yegangs/study1/user/SiteUser.java
@@ -0,0 +1,23 @@
+package com.yegangs.study1.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
---
+++ src/main/java/com/yegangs/study1/user/UserService.java
@@ -0,0 +1,21 @@
+package com.yegangs.study1.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String password, String email) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
