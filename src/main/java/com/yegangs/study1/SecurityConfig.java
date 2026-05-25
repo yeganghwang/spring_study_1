aba2c5ac4a1a126ca5d5338d4bcd28aa2d9bf725
yeganghwang <ghkdtlahs@icloud.com>
2026-05-25 15:19:40 +0900

    로그인/로그아웃 기능 추가

--- src/main/java/com/yegangs/study1/SecurityConfig.java
+++ src/main/java/com/yegangs/study1/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.yegangs.study1;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +26,17 @@ public class SecurityConfig {
                 .headers(headers -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin(formLogin -> formLogin
+                    .loginPage("/user/login").defaultSuccessUrl("/"))
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new RequestMatcher() {
+                           @Override
+                           public boolean matches(HttpServletRequest request) {
+                               return request.getRequestURI().endsWith("/user/logout");
+                           }
+                        })
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
         ;
         return http.build();
     }
@@ -29,5 +44,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
