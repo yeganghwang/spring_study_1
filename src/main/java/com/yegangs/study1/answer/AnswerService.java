7b68f5c383202e4ebae077a97e626e213f4c50df
yeganghwang <ghkdtlahs@icloud.com>
2026-05-26 14:35:10 +0900

    답변 수정/삭제기능 추가

--- src/main/java/com/yegangs/study1/answer/AnswerService.java
+++ src/main/java/com/yegangs/study1/answer/AnswerService.java
@@ -1,16 +1,27 @@
 package com.yegangs.study1.answer;
 
+import com.yegangs.study1.question.DataNotFoundException;
 import com.yegangs.study1.question.Question;
 import com.yegangs.study1.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AnswerService {
     private final AnswerRepository answerRepository;
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
 
     public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
@@ -20,4 +31,15 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
+    }
+
 }
