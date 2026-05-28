4bc0afbc64ab596c8bfb8fec7d95cadf4c9e5add
yeganghwang <ghkdtlahs@icloud.com>
2026-05-28 14:40:49 +0900

    검색 기능 추가

--- src/main/java/com/yegangs/study1/question/QuestionRepository.java
+++ src/main/java/com/yegangs/study1/question/QuestionRepository.java
@@ -1,5 +1,6 @@
 package com.yegangs.study1.question;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
--- src/main/java/com/yegangs/study1/question/QuestionService.java
+++ src/main/java/com/yegangs/study1/question/QuestionService.java
@@ -1,9 +1,13 @@
 package com.yegangs.study1.question;
 
+import com.yegangs.study1.answer.Answer;
 import com.yegangs.study1.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.sql.ast.tree.select.SortSpecification;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import org.springframework.data.domain.PageRequest;
@@ -18,11 +22,31 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public Page<Question> getList(int page) {
+    private Specification<Question> search(String kw) {
+        return new Specification<Question>() {
+            private static final long serialVersioinUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),
+                        cb.like(u1.get("username"), "%" + kw + "%"),
+                        cb.like(a.get("content"), "%" + kw + "%"),
+                        cb.like(u2.get("username"), "%" + kw + "%")
+                );
+            }
+        };
+    }
+
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public Question getQuestion(Integer id) {
