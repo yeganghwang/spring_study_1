63dc21bb9c87ad5fc0febae05530545e05b32a99
yeganghwang <ghkdtlahs@icloud.com>
2026-05-31 16:39:02 +0900

    회원 서비스 추가

---
+++ src/main/java/com/example/study_1/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.study_1.service;
+
+import com.example.study_1.domain.Member;
+import com.example.study_1.repository.MemberRepository;
+import com.example.study_1.repository.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
