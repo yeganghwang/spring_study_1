21b2958c59887cca94d9a67301521b787cad8606
yeganghwang <ghkdtlahs@icloud.com>
2026-05-31 15:54:47 +0900

    회원 도메인과 리포지토리 만들기

--- src/main/java/com/example/study_1/controller/HelloController.java
+++ src/main/java/com/example/study_1/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.study_1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,58 @@ public class HelloController {
         model.addAttribute("title", "hello 출력하기");
         model.addAttribute("message", "안녕하세요. 메시지입니다.");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloSpring(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name,
+                          @RequestParam("age") Integer age,
+                          @RequestParam("male") boolean male) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge(age);
+        hello.setMale(male);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        private Integer age;
+        private boolean male;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public boolean isMale() {
+            return male;
+        }
+
+        public void setMale(boolean male) {
+            this.male = male;
+        }
     }
 }
